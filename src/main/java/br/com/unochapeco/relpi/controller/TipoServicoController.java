ae377cd9c1d2058deab9757ed8132ee83015ed31
Ruam Becker <becker.ruam@gmail.com>
2024-04-17 23:25:18 -0300

    Preparando o metodo insert para TIPOSERVICO

--- src/main/java/br/com/unochapeco/relpi/controller/TipoServicoController.java
+++ src/main/java/br/com/unochapeco/relpi/controller/TipoServicoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,10 @@ public class TipoServicoController {
         return ResponseEntity.ok().body(obj);
     }
     
+    @PostMapping
+    public ResponseEntity<TipoServico> insert(@RequestBody TipoServico obj){
+    	service.insert(obj);
+        return ResponseEntity.ok().body(obj);
+    }
     
 }
