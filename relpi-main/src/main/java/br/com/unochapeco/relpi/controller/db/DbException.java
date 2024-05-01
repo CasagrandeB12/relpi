ebc226c1503dff98c675b0c5fe36e21ed35ccd8e
Eduardo Farfus <eduardofarfus@gmail.com>
2024-05-01 19:19:05 -0300

    Delete relpi-main directory

--- relpi-main/src/main/java/br/com/unochapeco/relpi/controller/db/DbException.java
+++
@@ -1,9 +0,0 @@
-package br.com.unochapeco.relpi.controller.db;
-
-public class DbException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public DbException(String msg) {
-		super(msg);
-	}
-}
